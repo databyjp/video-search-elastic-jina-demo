8cf8e34b140d07d26a657580e31f08388f2ae316
JP Hwang <61258750+databyjp@users.noreply.github.com>
2026-06-23 12:45:07 +0100

    index creation

--- main.py
+++ main.py
@@ -9,3 +9,26 @@ client = Elasticsearch(
     api_key=os.getenv("ES_LOCAL_API_KEY"),
 )
 print(client.info())
+
+INDEX = "elastic-wizard"
+
+# RESET INDEX
+client.indices.delete(index=INDEX, ignore_unavailable=True)
+client.indices.create(
+    index=INDEX,
+    mappings={
+        "properties": {
+            "content_type": {"type": "keyword"},
+            "title": {"type": "text"},
+            "embedding": {
+                "type": "dense_vector",
+                "dims": 768,
+                "index": True,
+                "similarity": "cosine"
+            }
+        }
+    }
+)
+
+if client.indices.exists(index=INDEX):
+    print(f"Index '{INDEX}' successfully created")
